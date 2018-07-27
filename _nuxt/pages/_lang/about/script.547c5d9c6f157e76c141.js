f9d53e357bac06f89645fbb31706ed814a17edcc
heziode <heziode@protonmail.com>
2018-07-27 21:21:23 +0200

    8acc950

---
+++ _nuxt/pages/_lang/about/script.547c5d9c6f157e76c141.js
@@ -0,0 +1,1 @@
+webpackJsonp([48],{"+Fjj":function(t,e,o){"use strict";Object.defineProperty(e,"__esModule",{value:!0});e.default={head:function(){return{title:this.$t("routes.about.title")}},data:function(){return this.$store.commit("SET_CURRENT_LOCATION","about"),this.$store.commit("RESTORE_HERO"),{i18nPrefix:"routes.about."}}}}});
